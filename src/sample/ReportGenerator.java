09d71c98ec9912fc168069bd6952bb18e72a904c
Partizanin <topiciv1@yandex.ua>
2017-02-28 17:50:13 +0100

    working reportGenerator

--- src/sample/ReportGenerator.java
+++ src/sample/ReportGenerator.java
@@ -1,8 +1,13 @@
 package sample;
 
+import java.awt.*;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -14,21 +19,22 @@ import java.util.stream.Stream;
  */
 public class ReportGenerator {
     public static void main(String[] args) {
-        String path = new ReportGenerator().getPath();
+        ReportGenerator reportGenerator = new ReportGenerator();
+        reportGenerator.saveAndOpenResultFile();
 
-        readReportFile(path);
+
     }
 
-    public String getPath() {
+    private String getPath() {
         String path = String.valueOf(getClass().getProtectionDomain().getCodeSource().getLocation());
-        path += "sample/results/result.html";
         path = path.substring(6);
         return path;
     }
 
-    public static String  readReportFile(String path) {
-        StringBuffer buffer = new StringBuffer("");
+    private String readReportFile() {
+        StringBuilder buffer = new StringBuilder("");
         //read file into stream, try-with-resources
+        String path = getPath() + "sample/results/result.html";
         try (Stream<String> stream = Files.lines(Paths.get(path))) {
 
             stream.forEach(buffer::append);
@@ -39,5 +45,27 @@ public class ReportGenerator {
         return buffer.toString();
     }
 
-}/*D:\Illia\Java\Projects\DateTimeCounter\out\production\DateTimeCounter\src\sample\results
-   D:\Illia\Java\Projects\DateTimeCounter\out\production\DateTimeCounter\sample\results*/
+    private StringBuilder createNewReport() {
+        StringBuilder buffer = new StringBuilder(readReportFile());
+
+        int i = buffer.indexOf("id=\"1\">") + 7;
+        buffer.insert(i, "123");
+        System.out.println(buffer.toString());
+        return buffer;
+    }
+
+    private void saveAndOpenResultFile() {
+        List<String> lines = Collections.singletonList(createNewReport().toString());
+        String path = getPath() + "\\sample\\results\\myResult.html";
+        Path file = Paths.get(path);
+        System.out.println(path);
+
+        try {
+            Files.write(file, lines, Charset.forName("UTF-8"));
+            Desktop.getDesktop().browse(file.toUri());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
