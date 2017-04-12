d9ca94c3a00cbacf267a3d2a84041e73c5c0e43c
Partizanin <topiciv1@yandex.ua>
2017-04-12 17:05:44 +0200

    add readme file change title text

--- src/java/sample/Main.java
+++ src/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Дата до кінця візи");
+        primaryStage.setTitle("Калькулятор візи");
         primaryStage.setScene(new Scene(root, 439, 469));
         primaryStage.setResizable(false);
         primaryStage.show();
